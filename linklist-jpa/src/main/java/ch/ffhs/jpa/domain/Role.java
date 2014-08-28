a138cc1449e67502f453a421e27d71981866c3c4
Patrick Bösch <patrick.boesch@vision.ch>
2014-08-28 16:43:35 +0200

    kleine Korrekturen

--- linklist-jpa/src/main/java/ch/ffhs/jpa/domain/Role.java
+++ linklist-jpa/src/main/java/ch/ffhs/jpa/domain/Role.java
@@ -5,8 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity(name="role")
