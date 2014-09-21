9aae3538385637a6bfa6e942d6944b462c2c1c8c
Sandro Dallo <sandro@dallo.biz>
2014-09-21 19:30:09 +0200

    Rollen Übersicht (noch nicht fertig)

--- linklist-jsf/src/main/java/ch/ffhs/jsf/controller/RoleController.java
+++ linklist-jsf/src/main/java/ch/ffhs/jsf/controller/RoleController.java
@@ -1,16 +1,30 @@
 package ch.ffhs.jsf.controller;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 
 import ch.ffhs.jpa.domain.Role;
 import ch.ffhs.jpa.service.intf.RoleService;
 
 @ManagedBean
-public class RoleController {
+@ViewScoped
+public class RoleController implements Serializable{
 	private Role role = new Role();
+	private List<Role> roles;
+	private List<Role> filteredRoles;
+
+	public List<Role> getRoles() {
+		if(roles == null){
+			roles = roleService.getAll();
+		}
+		return roles;
+	}
 
 	@EJB
 	private RoleService roleService;
@@ -33,5 +47,13 @@ public class RoleController {
 	
 	public void setRoleService(RoleService roleService) {
 		this.roleService = roleService;
+	}
+
+	public List<Role> getFilteredRoles() {
+		return filteredRoles;
+	}
+
+	public void setFilteredRoles(List<Role> filteredRoles) {
+		this.filteredRoles = filteredRoles;
 	}
 }
