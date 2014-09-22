6fadefc11286e7536eac1594ef59a77a1d3f3973
Sandro Dallo <sandro@dallo.biz>
2014-09-22 23:02:26 +0200

    Search funktioniert einfach nicht

--- linklist-jsf/src/main/java/ch/ffhs/jsf/controller/RoleController.java
+++ linklist-jsf/src/main/java/ch/ffhs/jsf/controller/RoleController.java
@@ -1,12 +1,15 @@
 package ch.ffhs.jsf.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.view.ViewScoped;
 
 import ch.ffhs.jpa.domain.Role;
@@ -17,7 +20,7 @@ import ch.ffhs.jpa.service.intf.RoleService;
 public class RoleController implements Serializable{
 	private Role role = new Role();
 	private List<Role> roles;
-	private List<Role> filteredRoles;
+	private String searchFilter = "";
 
 	public List<Role> getRoles() {
 		if(roles == null){
@@ -28,8 +31,13 @@ public class RoleController implements Serializable{
 
 	@EJB
 	private RoleService roleService;
+	private List<Role> filteredRoles;
 	
 	
+	public void setFilteredRoles(List<Role> filteredRoles) {
+		this.filteredRoles = filteredRoles;
+	}
+
 	public Role getRole() {
 		return role;
 	}
@@ -53,7 +61,8 @@ public class RoleController implements Serializable{
 		return filteredRoles;
 	}
 
-	public void setFilteredRoles(List<Role> filteredRoles) {
-		this.filteredRoles = filteredRoles;
+	public String getSearchFilter() {
+		return searchFilter;
 	}
+
 }
